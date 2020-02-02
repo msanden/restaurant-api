e1d8973c2ccc25d3106944be9965ce394dce4f3e
Manuel Sande <mnlsande22@gmail.com>
2020-02-02 15:37:27 -0500

    Ad line for clarity

--- src/main/java/models/Review.java
+++ src/main/java/models/Review.java
@@ -3,13 +3,15 @@ package models;
 import java.util.Objects;
 
 public class Review {
+
   private String content;
   private String writtenBy;
   private int rating;
   private int id;
-  private int restaurantId; //will be used to connect Restaurant to Review (one-to-many)
+  private int restaurantId; // connects Restaurant to Review (one-to-many)
 
   public Review(String content, String writtenBy, int rating, int restaurantId) {
+
     this.content = content;
     this.writtenBy = writtenBy;
     this.rating = rating;
